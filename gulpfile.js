68bfb09a53125d6531735bcd804a318f5e97f7f0
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2016-08-28 11:28:17 -0500

    Installed gulp-rev, gulp-uglify and gulp-usemin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var historyApiFallback = require('connect-history-api-fallback');
+var rev = require('gulp-rev');
+var uglify = uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+
+gulp.task('build', ['usemin', 'html', 'json']);
 
 gulp.task('default', ['serve']);
+
+gulp.task('html', function() {
+  gulp.src('dev/modules/**/*.html')
+  .pipe(gulp.dest('prod/modules'));
+});
+
+gulp.task('json', function() {
+  gulp.src('dev/json/*.json')
+  .pipe(gulp.dest('prod/json'));
+});
 
 gulp.task('serve', function() {
 
@@ -13,4 +28,12 @@ gulp.task('serve', function() {
 
   gulp.watch("dev/*.html").on('change', browserSync.reload);
   gulp.watch("dev/**/*.js").on('change', browserSync.reload);
+});
+
+gulp.task('usemin', function() {
+  gulp.src('dev/index.html')
+  .pipe(usemin({
+    scripts: [ uglify(), rev() ]
+  }))
+  .pipe(gulp.dest('prod'));
 });
